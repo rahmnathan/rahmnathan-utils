6f8871689a34439077ecf053365332bc22deade4
nathan <rahm.nathan@gmail.com>
2019-10-21 17:28:15 -0500

    Removed file-tree walk as apache file monitor is already recursive.

--- directory-monitor/src/main/java/com/github/rahmnathan/directory/monitor/DirectoryMonitor.java
+++ directory-monitor/src/main/java/com/github/rahmnathan/directory/monitor/DirectoryMonitor.java
@@ -6,11 +6,8 @@ import org.apache.commons.io.monitor.FileAlterationObserver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
 import java.nio.file.*;
-import java.nio.file.attribute.BasicFileAttributes;
 import java.util.*;
-import java.util.function.Consumer;
 
 public class DirectoryMonitor {
     private static final Logger logger = LoggerFactory.getLogger(DirectoryMonitor.class.getName());
@@ -22,32 +19,19 @@ public class DirectoryMonitor {
         FileAlterationMonitor monitor = new FileAlterationMonitor();
         FileAlterationListener listener = new DirectoryMonitorListener(monitor, observers);
 
-        Consumer<Path> register = p -> {
-            try {
-                Files.walkFileTree(p, new SimpleFileVisitor<>() {
-                    @Override
-                    public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) {
-                        logger.info("registering {} in watcher service", dir);
-                        FileAlterationObserver observer = new FileAlterationObserver(dir.toFile());
-                        observer.addListener(listener);
-                        monitor.addObserver(observer);
-                        paths.add(dir);
-                        return FileVisitResult.CONTINUE;
-                    }
-                });
-            } catch (IOException e) {
-                logger.error("Failure registering directory in directory monitor", e);
-            }
-        };
-
-        Arrays.stream(mediaPaths).map(Paths::get).forEach(register);
+        Arrays.stream(mediaPaths).map(Paths::get).forEach(p -> {
+            logger.info("registering {} in watcher service", p);
+            FileAlterationObserver observer = new FileAlterationObserver(p.toFile());
+            observer.addListener(listener);
+            monitor.addObserver(observer);
+            paths.add(p);
+        });
 
         try {
             monitor.start();
         } catch (Exception e){
             throw new RuntimeException(e);
         }
-
     }
 
     public Set<Path> getPaths() {
