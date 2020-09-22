a4d27dd60e26e2784ae0f55ee6a9bac7cdaa7015
nathan <rahm.nathan@gmail.com>
2020-09-21 22:46:59 -0500

    Reverted to LTS release of Java.

--- jenkins.groovy
+++ jenkins.groovy
@@ -3,7 +3,7 @@ node {
     def jdk
     stage('Setup') {
         mvnHome = tool 'Maven'
-        jdk = tool name: 'Java 14'
+        jdk = tool name: 'Java 11'
         env.JAVA_HOME = "${jdk}"
     }
     stage('Checkout') {
