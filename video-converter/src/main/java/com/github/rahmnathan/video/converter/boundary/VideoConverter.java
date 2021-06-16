92ac4b9b0164dc07d5cef53d59c7ff9437188f67
Nathan <rahm.nathan@protonmail.com>
2021-06-15 22:59:22 -0500

    Removed unused metric.

--- video-converter/src/main/java/com/github/rahmnathan/video/converter/boundary/VideoConverter.java
+++ video-converter/src/main/java/com/github/rahmnathan/video/converter/boundary/VideoConverter.java
@@ -1,11 +1,6 @@
 package com.github.rahmnathan.video.converter.boundary;
 
-import io.micrometer.core.instrument.Metrics;
-
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Supplier;
 
 public interface VideoConverter extends Supplier<String> {
-    AtomicInteger ACTIVE_CONVERSION_GAUGE = Metrics.gauge("handbrake.conversions.active", new AtomicInteger(0));
-
 }
