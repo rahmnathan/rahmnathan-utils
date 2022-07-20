dfdcdc79f58ab906e06f8884badb15981cba658b
Nathan <rahm.nathan@protonmail.com>
2022-07-19 21:01:10 -0700

    Removed logging logic and enable verbose handbrake logging.

--- video-converter-handbrake-kubernetes/src/main/java/com/github/rahmnathan/video/cast/handbrake/handbrake/HandbrakeServiceKubernetes.java
+++ video-converter-handbrake-kubernetes/src/main/java/com/github/rahmnathan/video/cast/handbrake/handbrake/HandbrakeServiceKubernetes.java
@@ -9,20 +9,12 @@ import io.fabric8.kubernetes.client.KubernetesClientBuilder;
 import io.fabric8.kubernetes.client.jdkhttp.JdkHttpClientFactory;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.MDC;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.*;
-import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
-import java.util.function.Consumer;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 @Slf4j
 @AllArgsConstructor
@@ -49,7 +41,8 @@ public class HandbrakeServiceKubernetes {
 
             List<String> args = List.of("-Z", conversionJob.getHandbrakePreset(),
                     "-i", conversionJob.getInputFile().getAbsolutePath(),
-                    "-o", conversionJob.getOutputFile().getAbsolutePath());
+                    "-o", conversionJob.getOutputFile().getAbsolutePath(),
+                    "-v");
 
             List<Volume> volumes = localmoviesPodOptional.get().getSpec().getVolumes().stream()
                     .filter(volume -> volume.getName().startsWith("media"))
@@ -104,8 +97,6 @@ public class HandbrakeServiceKubernetes {
                             job1.getStatus().getReady() != null &&
                             job1.getStatus().getReady() > 0, 5, TimeUnit.MINUTES);
 
-            CompletableFuture.runAsync(withMdc(new StreamConsumer(client.batch().v1().jobs().withName(podName).getLogInputStream(), log::info)));
-
             client.batch().v1().jobs().inNamespace(namespace).withName(podName).waitUntilCondition(job1 ->
                     job1 != null &&
                             job1.getStatus() != null &&
@@ -114,40 +105,6 @@ public class HandbrakeServiceKubernetes {
 
             conversionJob.getInputFile().delete();
             log.info("Job completed.");
-        }
-    }
-
-    private static Runnable withMdc(Runnable runnable) {
-        Map<String, String> mdc = MDC.getCopyOfContextMap();
-        return () -> {
-            MDC.setContextMap(mdc);
-            runnable.run();
-        };
-    }
-
-    private static class StreamConsumer implements Runnable {
-        private final Pattern pattern = Pattern.compile("\\d?\\d(?=.\\d\\d %)");
-        private final Consumer<String> consumer;
-        private final InputStream inputStream;
-
-        private StreamConsumer(InputStream inputStream, Consumer<String> consumer) {
-            this.inputStream = inputStream;
-            this.consumer = consumer;
-        }
-
-        @Override
-        public void run() {
-            Set<String> set = new HashSet<>();
-            new BufferedReader(new InputStreamReader(inputStream)).lines()
-                    .filter(s -> {
-                        Matcher matcher = pattern.matcher(s);
-                        if (matcher.find()) {
-                            return set.add(matcher.group());
-                        }
-
-                        return true;
-                    })
-                    .forEach(consumer);
         }
     }
 }
